8ce387ede907e5352e691d5ea7442b7503c1048e
naga4103 <naganaveena103@gmail.com>
2024-02-09 13:04:46 +0530

    Debouncing

---
+++ index.js
@@ -0,0 +1,19 @@
+let counter = 0;
+
+const getData = () => {
+  console.log("Fetching data...", counter++);
+};
+
+const debounce = function (fn, d) {
+  let timer;
+  return function () {
+    let context = this,
+      args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      getData.apply(context, arguments);
+    }, d);
+  };
+};
+
+const betterFunction = debounce(getData, 300);
